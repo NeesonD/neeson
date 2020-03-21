98dc71bf887bb084f9a64ab6a34b9ed6fed78c2b
neeson <d1043358774@gmail.com>
2020-03-21 23:03:18 +0800

    事件模式

--- common/src/main/java/com/neeson/common/autoconfigure/HttpAutoConfiguration.java
+++ common/src/main/java/com/neeson/common/autoconfigure/HttpAutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.neeson.common.autoconfigure;
 
 import com.neeson.common.http.RestTemplateConfiguration;
+import com.neeson.common.http.interceptor.JwtHeaderRequestInterceptor;
+import com.neeson.common.http.interceptor.LogRequestInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -13,6 +15,10 @@ import org.springframework.context.annotation.Import;
 @Import(RestTemplateConfiguration.class)
 public class HttpAutoConfiguration {
 
+    @Configuration
+    @Import({JwtHeaderRequestInterceptor.class, LogRequestInterceptor.class})
+    public static class ClientHttpRequestInterceptorConfiguration {
 
+    }
 
 }
